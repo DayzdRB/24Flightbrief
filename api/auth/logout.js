f072d3e6499b8e3b9f5680e97bf03c30572b38bb
DayzdRB <2008trev@gmail.com>
2026-07-15 08:25:34 -0500

    Update logout.js

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,1 +1,7 @@
+// POST /api/auth/logout
+const { clearSessionCookie } = require('../../lib/session');
 
+module.exports = async (req, res) => {
+  clearSessionCookie(res);
+  res.status(200).json({ ok: true });
+};
